e5fa15be2d3dceb39877f6359cc475e3d9fc40a0
jorgep19 <jorgelpaez19@gmail.com>
2014-04-07 17:54:10 -0400

    setup for database connection

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,19 @@
-var app = require('../app.js');
+var app = require('../app.js'),
+    pg = require('pg');
 
 app.get('/', function(req, res){
     res.render('index', { title: 'Express' });
+});
+
+app.get('/dbtest', function(req, res){
+
+    pg.connect( process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM sensor_type;', function(err, result) {
+            done();
+
+            if(err) return console.error(err);
+
+            res.json(result.rows);
+        });
+    });
 });
