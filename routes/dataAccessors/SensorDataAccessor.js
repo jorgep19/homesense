40db65c9577797933ec533cc50a38e022ad90a85
jorgep19 <jorgelpaez19@gmail.com>
2014-04-10 03:35:38 -0400

    almost done with refactoring of backend

--- routes/dataAccessors/SensorDataAccessor.js
+++ routes/dataAccessors/SensorDataAccessor.js
@@ -3,6 +3,7 @@ var constructor = function() {
     var pg = require('pg');
     var sensorDataAccessorInstance = {};
 
+    // gets all the supported sensor types from the database
     sensorDataAccessorInstance.getSensorTypes = function(sendResponse){
 
         pg.connect( process.env.DATABASE_URL, function(err, client, done) {
@@ -13,6 +14,51 @@ var constructor = function() {
             });
         });
     };
+
+    sensorDataAccessorInstance.insertDataRow = function(cusId, sensorData, res){
+        var queryTemplate = "INSERT INTO sensor_data (cusID, sensID, sdataValue, sdatarecordeddate) VALUES (" +
+            "$1," +
+            "$2," +
+            "$3," +
+            "$4)";
+        var inserts = [ cusId, sensorData.sensorId, sensorData.value, sensorData.date ];
+
+        pg.connect( process.env.DATABASE_URL, function(err, client, done) {
+            client.query(queryTemplate, inserts, function(err, result) {
+                done();
+
+                console.log('saving:' + result);
+
+                if(err) {
+                    res.send("Something went wrong")
+                }
+                res.write("inserted data for sensor ID" + sensorData.sensID);
+            });
+        });
+    };
+
+    sensorDataAccessorInstance.recordSensorReadings = function(data, res, insertRow) {
+
+        for(var i = 0; i < data.sensors.length; i++) {
+            insertRow(data.userId, data.sensors[i], res);
+        }
+    };
+
+    sensorDataAccessorInstance.getTemperatureData = function (sensorId, sendResponse){
+        var queryTemplate = "SELECT sdatavalue, sdatarecordeddate FROM sensor S, sensor_data D " +
+            "WHERE S.sensid = D.sensid AND S.sensid = $1 AND stypeid = 1" +
+            "ORDER BY sdatarecordeddate DESC";
+        var inserts = [ sensorId ];
+
+        pg.connect( process.env.DATABASE_URL, function(err, client, done) {
+            client.query(queryTemplate, inserts, function(err, result) {
+                done();
+
+                sendResponse(err, result.rows)
+            });
+        });
+    };
+
 
     return sensorDataAccessorInstance;
 }
--- routes/index.js
+++ routes/index.js
@@ -17,21 +17,74 @@ var checkSessionBeforeExec = function(requestHandler) {
 };
 
 // TEST ROUTES
+// -------------------------------------------------------------------------------------
 app.get('/', function(req, res){ res.send('Server is running') });
-app.get('/dbtest', checkSessionBeforeExec(sensorController.getSensorTypes) );
+app.get('/dbtest', sensorController.getSensorTypes );
 
 // PI ROUTES
-app.post('/api/pi/verify', piController.verifyPi);                                      // basic suppor
-// TODO implement app.post('/api/pi/update', );
-// TODO implement app.post('/api/pi/settings/update', );
-// TODO implement app.post('/api/pi/put/data', );
+// -------------------------------------------------------------------------------------
+// This URL creates a user account if possible
+// Expects a JSON of this form: { piCode: 19 }
+// Returns a JSON of this form: { hasErrors: false, messages: [], data: { piId: 21, userId: 19 } }
+// in that JSON hasErrors is a boolean that states if there was an error
+// and messages is an array of string that has either error messages
+// or a message saying that the operation was successful
+app.post('/api/pi/verify', piController.verifyPi);                                      // basic support
+
+// This URL creates a user account if possible
+// Expects a JSON of this form: { userId: 19, sensors : [ { sensorId: 28, value: 12, date: '2014-04-07 17:50:52.9741'}, { sensorId: 28, value: 12, date: '2014-04-07 17:50:52.9741'} ] }
+// Returns a log of the transaction that got executed
+app.post('/api/sensor/put/data', sensorController.recordSensorReadings);                      // working on
+
+
+// This URL provides an update for the pi sensors
+// Expects a JSON of this form: { piId: 21 }
+// Returns a JSON of this form: { hasErrors: false, messages: [], sensors: [ { sensid: 1, stypeid: 1, sensdesc: "Garage_Temp" }, { sensid: 1, stypeid: 1, sensdesc: "Garage_Temp" } ]
+app.post('/api/pi/update',  piController.getSensorUpdate);
 
 // CLIENTS ROUTES
+// -------------------------------------------------------------------------------------
+// This URL creates a user account if possible
+// Expects a JSON of this form: { email: 'dummie@foo.bar', password: '123' }
+// Returns a JSON of this form: { hasErrors: false, messages: [] }
+// in that JSON hasErrors is a boolean that states if there was an error
+// and messages is an array of string that has either error messages
+// or a message saying that the operation was successful
 app.post('/api/customer/register', userController.registerUser);                        // basic support
+
+// This URL starts session
+// Expects a JSON of this form: { email: 'dummie@foo.bar', password: '123' }
+// Returns a JSON of this form: { hasErrors: false, messages: [] }
+// in that JSON hasErrors is a boolean that states if there was an error
+// and messages is an array of string that has either error messages
+// or a message saying that the operation was successful
 app.post('/api/login/', userController.login);                                          // basic support
+
+// This URL ends the session of a user
+// Expects nothing
+// Returns a JSON of this form: { hasErrors: false, messages: [] }
+// in that JSON hasErrors is a boolean that states if there was an error
+// and messages is an array of string that has either error messages
+// or a message saying that the operation was successful
 app.post('/api/logout', checkSessionBeforeExec(userController.logout) );                // basic support
+
+// This URL ends the session of a user
+// Expects nothing
+// Returns a JSON of this form: { piDesc: 'piDescriptionString' }
+// in that JSON hasErrors is a boolean that states if there was an error
+// and messages is an array of string that has either error messages
+// or a message saying that the operation was successful
 app.post('/api/customer/genpicode', checkSessionBeforeExec(userController.genPiCode) ); // basic support
+
+// This URL ends the session of a user
+// Expects nothing
+// Returns a JSON of this form [ { "stypeid": 1, "stypedesc": "Temperature" }, { "stypeid": 2, "stypedesc": "Motion" } ]
 app.get('/api/sensor/get/types', sensorController.getSensorTypes);
 
+
+// This URL return the collection of all the data for an specific sensor
+// Expect a number with the sensor code at the end of the url
+// Returns a JSON of this form hasErrors: false, messages: [0], data: [ { sdatavalue: "12", sdatarecordeddate: "2014-04-07T21:50:52.974Z" }, { sdatavalue: "21", sdatarecordeddate: "2014-04-07T21:50:52.974Z" } ] }
+app.get('/api/get/temperature/data/:sensorId', sensorController.getTemperatureData);
+
 // TODO implement app.get('/api/customer/get/summary/data', );
-// TODO implement app.get('/api/get/temperature/data', );
